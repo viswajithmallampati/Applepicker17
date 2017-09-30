078effd09893f7bff1e3a6d731f3100ba57a3524
Viswajith Mallampati <31782362+viswajithmallampati@users.noreply.github.com>
2017-09-29 20:14:28 -0400

    ApplePicker

---
+++ Assets/ApplePicker.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApplePicker : MonoBehaviour {
+
+	public GameObject basketPrefab;
+	public int numBaskets = 3;
+	public float basketBottomY = -14f;
+	public float basketSpacingY = 2f;
+	public List<GameObject> basketList;
+
+	// Use this for initialization
+	void Start () {
+		basketList = new List<GameObject> ();
+		for(int i = 0; i < numBaskets; i++) 
+		{
+			GameObject tBasketGO = Instantiate (basketPrefab) as GameObject;
+			Vector3 pos = Vector3.zero;
+			pos.y = basketBottomY + (basketSpacingY * i);
+			tBasketGO.transform.position = pos;
+			basketList.Add(tBasketGO);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void AppleDestroyed()
+	{
+		GameObject[] tAppleArray = GameObject.FindGameObjectsWithTag ("Apple");
+		foreach (GameObject tGO in tAppleArray) {
+			Destroy (tGO);
+		}
+		int basketIndex = basketList.Count - 1;
+		GameObject tBasketGO = basketList [basketIndex];
+		basketList.RemoveAt (basketIndex);
+		Destroy (tBasketGO);
+
+		if (basketList.Count == 0) {
+			Application.LoadLevel ("Placeholder-DELETE_ME");
+		}
+	}
+}
---
+++ Assets/HighScore.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighScore : MonoBehaviour {
+
+	static public int score = 1000;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GUIText gt = this.GetComponent<GUIText> ();
+		gt.text = "High Score: " + score;
+
+		if (score > PlayerPrefs.GetInt ("ApplePickerHighScore")) {
+			PlayerPrefs.SetInt ("ApplePickerHighScore", score);
+		}
+	}
+}
